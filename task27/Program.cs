08440b155ae346092394373c127c48bf196c8db1
Gennady <genchik111@yandex.ru>
2023-04-02 12:39:28 +0300

    Add task27

---
+++ task27/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Введите любоу число: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.Write(SumSimbols(ConvertToArray(number)));
+
+int[] ConvertToArray(int number)
+{
+   int length = GetLengthNumber(number);
+   int[] result = new int[length];
+   for (int i = 0; i < length; i++)
+   {
+      result[i] = number % 10;
+      number = number / 10;
+   }
+   return result;
+}
+
+int GetLengthNumber(int number)
+{
+   int length = 0;
+   while (number != 0)
+   {
+      number = number / 10;
+      length++;
+   }
+   return length;
+}
+
+int SumSimbols(int[] numbers)
+{
+   int result = 0;
+   for (int i = 0; i < numbers.Length; i++)
+   {
+      result += numbers[i];
+   }
+   return result;
+}
