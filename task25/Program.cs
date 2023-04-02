5484026468e6edeafb191738e1831e9e433ba89c
Gennady <genchik111@yandex.ru>
2023-04-02 12:25:52 +0300

    Add task25

---
+++ task25/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число A: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = int.Parse(Console.ReadLine());
+
+Console.Write($"-> {PowerNumber(A, B)}");
+
+int PowerNumber(int number, int exponent)
+{
+   int result = 1;
+   for (int i = 1; i <= exponent; i++)
+   {
+      result *= number;
+   }
+   return result;
+}
